223f9bf1bdbc50a1a5e514a0de0d6a76ee02bb6f
Ryan Butler <thebutlah@gmail.com>
2018-09-23 15:07:45 -0400

    Began building object framework

---
+++ agents/agent.py
@@ -0,0 +1,22 @@
+# Python 2-3 Compatibility
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+
+
+class Agent(object):
+    """Represents an agent that interacts with an environment."""
+
+    def __init__(self):
+        """Constructs an agent and initializes it."""
+        raise NotImplementedError
+
+    def train_step(self):
+        """Trains the agent for a single step of the simulation."""
+        raise NotImplementedError
+
+    def pick_action(self, trajectory):
+        raise NotImplementedError
+
+    # TODO: finish agent api
