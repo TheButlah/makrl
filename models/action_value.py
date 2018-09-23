7d8d98314a56c5f03d0922647533ce6e05bd5c71
Ryan Butler <thebutlah@gmail.com>
2018-09-23 17:48:51 -0400

    Added ActionValue and StateValue ABC's

--- models/action_value.py
+++ models/action_value.py
@@ -27,27 +27,38 @@ class ActionValue(with_metaclass(ABCMeta, Model)):
 
     @abstractmethod
     def predict_q(self, state, action):
-        """Predicts the action-value q for a batch of state-action pairs.
+        """Predicts the action-value q for a batch of state-action pairs, or
+        greedily selects the best action for a state and then returns the value
+        of that state-action pair.
 
         Args:
             state:  A batched representation of the state of the environment as
                 a numpy array.
-            action:  A batched representation of the action to take.
+            action:  A batched representation of the action to take. If `None`,
+                the action will be automatically selected greedily, such that
+                the action will give the maximum value for that state. In that
+                case, the action selected will be returned along with the value.
 
         Returns:
-            A batch of action-values as a numpy array.
+            A batch of action-values as a numpy array. If `action` was null,
+            the result will instead be a tuple of the action-values and the
+            selected actions.
         """
         pass
 
     @abstractmethod
-    def predict_v(self, state):
-        """Predicts the state-value v for a batch of states.
+    def update_q(self, target_return, state, action):
+        """Informs the model of updated action-values for a given batch of
+        state-action pairs. Note that `target_return` is capable of being an
+        n-step return.
 
         Args:
-            state:  A batched representation of the state of the environment as
-                a numpy array.
-
-        Returns:
-            A batch of state-values as a numpy array.
+            target_return:  A batch of action-values as a numpy array. These
+                values should be a new, ideally better estimate of the return at
+                the given state-action pair. The model will use this to improve.
+            state:  A batched representation of the state of the environment for
+                which the value function will be updated, as a numpy array.
+            action:  A batched representation of the actions for which the value
+                function will be updated.
         """
         pass
