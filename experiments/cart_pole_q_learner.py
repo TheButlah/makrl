a8a0350643019bb5e68729c3a63374b68af1b47b
Ryan Butler <thebutlah@gmail.com>
2018-10-03 17:46:32 -0400

    Finished cmdline documentation for argparse

--- experiments/cart_pole_q_learner.py
+++ experiments/cart_pole_q_learner.py
@@ -10,7 +10,7 @@ import os
 import time
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH = os.path.join(BASE_DIR, 'saved', '')
+SAVE_DIR = None
 
 
 def main():
@@ -21,20 +21,21 @@ def main():
     default='%s' % time.strftime('cart-pole-q-learner_%Y%m%d_%H%M%S'),
     help="The name of the experiment being run. Used to identify different "
          "experiments of the same type. Will by default be named based on the "
-          "current time.")
+         "current time.")
   parser.add_argument('-s', '--save-to', metavar='PATH',
     type=str,
     help="A path to the directory to save the results of the experiment. "
-         "Otherwise, the results will be saved in a new directory named the "
-         "same as the --name argument in the current working directory.")
+         "If not specified, the results will be saved in a new directory named "
+         "the same as the --name argument in the \"experiments\" directory.")
   parser.add_argument("-l", "--load-from", metavar='PATH',
-    type=str, nargs='?',
+    type=str,
     help="This flag will cause the model to be loaded from a prior experiment "
-         "rather than starting fresh. If the "
-         "this experiment. This is useful when loading a (partially) trained "
-         "agent.")
+         "located at the given path, rather than starting fresh. This is "
+         "useful when loading a (partially) trained agent.")
   args = parser.parse_args()
+
   print(args)
+  # TODO: Use `args.name` to figure out the correct default paths
 
 
 if __name__ == "__main__":
